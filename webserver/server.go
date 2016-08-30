29eb96dada648da873faf5226b21465087cbf642
FilippiLE <leo-ubp@hotmail.com>
2016-08-30 17:01:40 -0300

    Version inicial

--- webserver/server.go
+++ webserver/server.go
@@ -1,6 +1,35 @@
 package main
+
+import(
+"fmt"
+//"os"
+"runtime"
+"github.com/gin-gonic/gin"
+)
+
+var(
+Router *gin.Engine
+)
 	
 func main() {
-	
+	startApp()
 }
+
+func startApp(){
+configureRuntime()
+mapURLsToControllers()
+Router = gin.Default()
+Router.RedirectFixedPath = false 
+Router.RedirectTrailingSlash = false
+Router.Run(":8080")
+}
+
+func configureRuntime(){
+	numCPU := runtime.NumCPU()
+	fmt.Println(numCPU)
+	runtime.GOMAXPROCS(numCPU)
+}
+
+
+
 
--- webserver/url_mappings.go
+++ webserver/url_mappings.go
@@ -1,2 +1,14 @@
 package main
-	
+
+import(
+"github.com/gin-gonic/gin"
+"net/http"
+)
+
+func mapURLsToControllers(){
+	Router.GET("/ping",Ping)
+}
+
+func Ping(c *gin.Context){
+c.String(http.StatusOK, "pong")
+}
